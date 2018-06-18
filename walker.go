1b851ddf84d6c6b8e6c7a114662e7eafc39ed468
mathetake <cz.rk.t0415y.g@gmail.com>
2018-06-18 21:26:42 +0900

    add missing files

---
+++ walker.go
@@ -0,0 +1,85 @@
+package walker
+
+import (
+	"math/rand"
+)
+
+type Sampler struct {
+	prob  map[int]float64
+	alias map[int]int
+	size  int
+	seed  float64
+}
+
+func (s *Sampler) Generate() int {
+	idx := rand.Intn(s.size)
+	x := rand.Float64()
+	if x < s.prob[idx] {
+		return idx
+	} else {
+		return s.alias[idx]
+	}
+}
+
+// GetSampler returns a sampler with Walker's sampling method.
+// The following is implemented according to `A Practical Version of Vose's Algorithm` in
+// http://www.keithschwarz.com/darts-dice-coins
+func GetSampler(ws []float64, normalize bool) *Sampler {
+	// initialize a sampler
+	s := &Sampler{}
+	s.size = len(ws)
+	var size64 = float64(s.size)
+	s.prob = make(map[int]float64, s.size)
+	s.alias = make(map[int]int, s.size)
+
+	// create two work lists
+	var small, large []int
+
+	// if normalize == true, normalize the given weights so that they sum up to 1
+	var z float64 = 1
+	if normalize {
+		z = 0
+		for _, p := range ws {
+			z += p
+		}
+	}
+
+	// scale all of weights by `size`
+	for i := range ws {
+		ws[i] = (ws[i] * size64) / z
+	}
+
+	for i, p := range ws {
+		if p < 1 {
+			small = append(small, i)
+		} else {
+			large = append(large, i)
+		}
+	}
+
+	var l, g int
+	for len(small) > 0 {
+		small, l = pop(small)
+		large, g = pop(large)
+
+		s.prob[l] = ws[l]
+		s.alias[l] = g
+		ws[g] = (ws[g] + ws[l]) - 1
+
+		if ws[g] < 1 {
+			small = append(small, g)
+		} else {
+			large = append(large, g)
+		}
+	}
+
+	for len(large) > 0 {
+		large, g = pop(large)
+		s.prob[g] = 1
+	}
+	return s
+}
+
+func pop(s []int) ([]int, int) {
+	return s[:len(s)-1], s[len(s)-1]
+}
---
+++ walker_test.go
@@ -0,0 +1,119 @@
+package walker
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/bmizerany/assert"
+)
+
+const (
+	tol = 10e-5
+	num = 10e7
+)
+
+func TestGenerate(t *testing.T) {
+	tc := []struct {
+		ws        []float64
+		normalize bool
+	}{
+		{
+			[]float64{0.5, 0.5},
+			false,
+		},
+		{
+			[]float64{0.2, 0.2, 0.2, 0.2, 0.2},
+			false,
+		},
+		{
+			[]float64{0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2},
+			true,
+		},
+		{
+			[]float64{0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5},
+			true,
+		},
+		{
+			[]float64{0.5, 1.5, 100.3},
+			true,
+		},
+		{
+			[]float64{23424124.23, 0.5},
+			true,
+		},
+	}
+
+	for i, c := range tc {
+		// intentionally initialize seeds
+		rand.Seed(time.Now().UnixNano())
+		eRatio := make(map[int]float64, len(c.ws))
+
+		var z float64 = 1
+		if c.normalize {
+			z = 0
+			for j := range c.ws {
+				z += c.ws[j]
+			}
+		}
+
+		for j := range c.ws {
+			eRatio[j] = c.ws[j] / z
+		}
+
+		s := GetSampler(c.ws, c.normalize)
+		ratio := make(map[int]float64, len(c.ws))
+		for j := 0; j < num; j++ {
+			x := s.Generate()
+			ratio[x] += 1 / num
+		}
+		for j := range ratio {
+			if ratio[j]-eRatio[j] > tol {
+				t.Fatalf("%v-th test failed with \n ratio:%v \nexpected: %v", i, ratio, eRatio)
+			}
+		}
+	}
+}
+
+// check if GetSampler doesn't cause panic
+func TestGetSampler(t *testing.T) {
+	tc := []struct {
+		ws        []float64
+		normalize bool
+	}{
+		{
+			[]float64{0.5, 0.5},
+			false,
+		},
+		{
+			[]float64{0.2, 0.2, 0.2, 0.2, 0.2},
+			false,
+		},
+		{
+			[]float64{0.5, 1.5, 100.3},
+			true,
+		},
+		{
+			[]float64{23424124.23, 0.5},
+			true,
+		},
+	}
+
+	for _, c := range tc {
+		GetSampler(c.ws, c.normalize)
+	}
+}
+
+func TestPop(t *testing.T) {
+	tc := [][]int{
+		{1, 1, 1, 1, 1, 1, 1, 10},
+		{1, 1, 1, 0},
+		{1},
+	}
+
+	for _, c := range tc {
+		as, p := pop(c)
+		assert.Equal(t, len(c)-1, len(as))
+		assert.Equal(t, c[len(c)-1], p)
+	}
+}
